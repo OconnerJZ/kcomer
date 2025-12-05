b1d7d903c0fadc8b2a79e175d854f1ee4ee79559
bjaramillo <bryantjz95@gmail.com>
2025-12-05 08:09:12 -0600

    delete helmet

--- src/components/layout/GeneralContent.jsx
+++ src/components/layout/GeneralContent.jsx
@@ -1,16 +1,9 @@
-import SEO from "@Components/SEO";
+
 import { Box } from "@mui/material";
 
 const GeneralContent = ({ children, title, url, description, image, type }) => {
   return (
     <Box component="div">
-      <SEO
-        title={title}
-        url={url}
-        description={description}
-        image={image}
-        type={type}
-      />
       {children}
     </Box>
   );
