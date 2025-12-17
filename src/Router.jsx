d24993a2fa9ce80d0cc2b1a56d28c44a7909641f
bjaramillo <bryantjz95@gmail.com>
2025-12-16 23:05:01 -0600

    feature: owner dashboard fixes

--- src/Router.jsx
+++ src/Router.jsx
@@ -3,18 +3,16 @@ import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
 import Layout from "@Components/layout/Layout";
 import Explorar from "@Pages/Explorar";
 import Nosotros from "@Pages/Nosotros";
-import RegisterBusiness from "@Pages/RegisterBusiness";
+
 import ScrollToTop from "@Components/ScrollToTop";
 import Pedidos from "@Pages/Pedidos";
 import Login from "@Pages/Login";
 import MisOrdenes from "@Pages/MisOrdenes";
-import BusinessDashboard from "@Pages/BusinessDashboard";
+
 import Perfil from "@Pages/Perfil";
 import { useAuth } from "@Context/AuthContext";
 import LandingRegister from "@Pages/LandingRegister";
 import OwnerDashboard from "@Pages/OwnerDashboard";
-import OwnerMenu from "@Pages/OwnerMenu";
-import OwnerReports from "@Pages/OwnerReports";
 
 // Componente para rutas protegidas
 const ProtectedRoute = ({ children }) => {
@@ -71,35 +69,11 @@ const Router = () => {
           />
 
           <Route
-            path="business-dashboard"
-            element={
-              
-                <BusinessDashboard />
-              
-            }
-          />
-          <Route
             path="owner"
             element={
-          
+              <ProtectedRoute>
                 <OwnerDashboard />
-              
-            }
-          />
-          <Route
-            path="owner-menu"
-            element={
-          
-                <OwnerMenu />
-              
-            }
-          />
-          <Route
-            path="owner-r"
-            element={
-          
-                <OwnerReports />
-              
+              </ProtectedRoute>
             }
           />
         </Route>
