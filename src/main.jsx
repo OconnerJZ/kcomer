eb87bc298a088f462e480eb86c875d7b0bbaa564
OconnerJZ <56703068+OconnerJZ@users.noreply.github.com>
2026-08-25 23:14:13 -0600

    refactor: consume app store directly

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./app/App.jsx";
 import "./index.css";
-import { store } from "@Config/redux/store.js";
+import { store } from "@App/store/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
