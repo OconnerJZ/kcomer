9c6c416ac7177325e9fb438c917edf6c6ae5f5c9
OconnerJZ <56703068+OconnerJZ@users.noreply.github.com>
2026-08-25 22:24:13 -0600

    refactor: point entrypoint to app composition root

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
 import { Provider } from "react-redux";
+import App from "./app/App.jsx";
+import "./index.css";
 import { store } from "@Config/redux/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
