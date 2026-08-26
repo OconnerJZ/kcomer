fc03a900debcc3486df1b0d683c11848260d88e6
OconnerJZ <56703068+OconnerJZ@users.noreply.github.com>
2026-08-25 23:39:19 -0600

    refactor: move parallax into explore feature

---
+++ src/features/explore/components/Parallax.jsx
@@ -0,0 +1,24 @@
+import { Box } from "@mui/material";
+import PropTypes from "prop-types";
+
+const Parallax = ({ bg, children, stx }) => {
+  return (
+    <Box
+      component="div"
+      className="parallax"
+      sx={{
+        backgroundImage: `url(${bg})`,
+        ...stx,
+      }}
+    >
+      {children}
+    </Box>
+  );
+};
+
+Parallax.propTypes = {
+  bg: PropTypes.string.isRequired,
+  children: PropTypes.element,
+};
+
+export default Parallax;
