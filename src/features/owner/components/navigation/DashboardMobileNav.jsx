a24db67386b0cb5b6234e15ab6101d7cdb83d955
OconnerJZ <56703068+OconnerJZ@users.noreply.github.com>
2026-09-06 19:40:29 -0600

    fix: make mobile owner navigation scrollable

--- src/features/owner/components/navigation/DashboardMobileNav.jsx
+++ src/features/owner/components/navigation/DashboardMobileNav.jsx
@@ -13,45 +13,67 @@ const DashboardMobileNav = ({
   onTabChange,
   pendingOrders = 0,
   allowedTabs = [0, 1, 2, 3],
-}) => (
-  <Paper
-    sx={{
-      width: "100%",
-      height: "9%",
-      position: "fixed",
-      bottom: 0,
-      left: 0,
-      zIndex: 1000,
-      display: { xs: "flex", sm: "flex", md: "none" },
-      borderTop: "1px solid #e0e0e0",
-      backgroundColor: "#fff",
-    }}
-    elevation={8}
-  >
-    <BottomNavigation
-      value={activeTab}
-      onChange={(_event, newValue) => onTabChange(newValue)}
-      showLabels
+}) => {
+  const tabs = getVisibleDashboardTabs(allowedTabs, pendingOrders);
+
+  return (
+    <Paper
       sx={{
         width: "100%",
-        "& .MuiBottomNavigationAction-root": { minWidth: "auto" },
+        position: "fixed",
+        bottom: 0,
+        left: 0,
+        zIndex: 1000,
+        display: { xs: "block", md: "none" },
+        borderTop: "1px solid",
+        borderColor: "divider",
+        backgroundColor: "background.paper",
+        pb: "env(safe-area-inset-bottom)",
+        overflowX: "auto",
+        scrollbarWidth: "none",
+        "&::-webkit-scrollbar": { display: "none" },
       }}
+      elevation={3}
     >
-      {getVisibleDashboardTabs(allowedTabs, pendingOrders).map((tab) => (
-        <BottomNavigationAction
-          key={tab.id}
-          value={tab.id}
-          label={tab.mobileLabel}
-          icon={tab.badge ? (
-            <Badge badgeContent={tab.badge} color="error" max={99}>
-              {DASHBOARD_ICONS[tab.icon]}
-            </Badge>
-          ) : DASHBOARD_ICONS[tab.icon]}
-        />
-      ))}
-    </BottomNavigation>
-  </Paper>
-);
+      <BottomNavigation
+        value={activeTab}
+        onChange={(_event, newValue) => onTabChange(newValue)}
+        showLabels
+        sx={{
+          minWidth: Math.max(420, tabs.length * 76),
+          height: 64,
+          justifyContent: "flex-start",
+          "& .MuiBottomNavigationAction-root": {
+            minWidth: 76,
+            maxWidth: 92,
+            px: 0.75,
+          },
+          "& .MuiBottomNavigationAction-label": {
+            fontSize: "0.68rem",
+            whiteSpace: "nowrap",
+          },
+          "& .Mui-selected .MuiBottomNavigationAction-label": {
+            fontSize: "0.7rem",
+            fontWeight: 600,
+          },
+        }}
+      >
+        {tabs.map((tab) => (
+          <BottomNavigationAction
+            key={tab.id}
+            value={tab.id}
+            label={tab.mobileLabel}
+            icon={tab.badge ? (
+              <Badge badgeContent={tab.badge} color="error" max={99}>
+                {DASHBOARD_ICONS[tab.icon]}
+              </Badge>
+            ) : DASHBOARD_ICONS[tab.icon]}
+          />
+        ))}
+      </BottomNavigation>
+    </Paper>
+  );
+};
 
 DashboardMobileNav.propTypes = {
   activeTab: PropTypes.number.isRequired,
